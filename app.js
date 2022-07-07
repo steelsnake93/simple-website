436e2ea3a9ca9bbbeda5711a90488813d16050fc
steelsnake93 <nikolanikotasev@protonmail.com>
2022-07-07 09:38:39 +0200

    add text element using javascript

--- app.js
+++ app.js
@@ -14,3 +14,14 @@ switcher.addEventListener('click', function() {
     console.log('current class name: ' + className);
 
 });
+
+let my_element = document.createElement('h1');
+let my_span = document.createElement('span');
+
+my_span.innerText = 'This is my World';
+
+my_element.appendChild(my_span);
+
+my_element.classList.add('costume_class');
+
+document.body.appendChild(my_element);
